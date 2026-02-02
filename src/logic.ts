93f8804aeffa7a004d15d1acb0b686ced739efdc
市川凌大 <ichikawaryohta@MacBook-Air.local>
2026-02-02 13:33:54 +0900

    fix: null値の追加やクォート漏れを修正しました。

--- src/logic.ts
+++ src/logic.ts
@@ -1,10 +1,10 @@
-import type { Operator } from "./types"
+import type { Operator } from "./types";
 
 export const calculate = (a: number, b: number, operator: Operator): number | 'Error' => {
     switch (operator) {
-        case '+': return a + b
-        case '-': return a - b
-        case '×': return a * b
+        case '+': return a + b;
+        case '-': return a - b;
+        case '×': return a * b;
         case '÷': return b === 0 ? 'Error' : a / b; // ゼロ除算のエラーハンドリング
         default: return b;
     }
