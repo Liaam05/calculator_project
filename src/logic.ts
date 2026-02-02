bd1c7f14875f2e728bc1eed9cd40fa0661dab092
市川凌大 <ichikawaryohta@MacBook-Air.local>
2026-02-02 13:07:59 +0900

    add: logic.tsファイルに計算ロジック関数を作った

---
+++ src/logic.ts
@@ -0,0 +1,11 @@
+import type { Operator } from "./types"
+
+export const calculate = (a: number, b: number, operator: Operator): number | 'Error' => {
+    switch (operator) {
+        case '+': return a + b
+        case '-': return a - b
+        case '×': return a * b
+        case '÷': return b === 0 ? 'Error' : a / b; // ゼロ除算のエラーハンドリング
+        default: return b;
+    }
+};
